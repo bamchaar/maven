513fbbbc26d198ec2c17a95a538d5db49295d0b7
DMVTCSTU <42882087+bamchaar@users.noreply.github.com>
2023-03-10 07:48:45 -0500

    Update script.groovy
    fixed script

--- script.groovy
+++ script.groovy
@@ -4,9 +4,11 @@ def BuildJar(){
     sh'mvn package'
 }
 def BuildImage(){
-    echo 'Build Docker image'
-    echo "Build version ${NEW_VERSION}"
-    sh'mvn package'
+    echo 'Building Docker image'
+	withCredentials([usernamePassword(credentialsId: 'tcdmvPipeLine',passwordVariable: 'PASS' , usernameVariable: 'USER')]){
+	sh 'docker build -t tcdmv/mvn-app:java-mvn-2.0 .'
+	sh "echo $PASS | docker login -u $USER --password-stdin"
+	sh 'docker push tcdmv/mvn-app:java-mvn-2.0'
 }
 def TestApp(){
     echo 'Testing stage'
