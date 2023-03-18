61cebe1d22326e180c86b3f0b1954f9aa25c791e
Abdel <bamchaar@gmail.com>
2023-03-18 14:55:34 -0400

    updated

--- script.groovy
+++ script.groovy
@@ -1,9 +1,9 @@
 def buildImage() {
     echo 'Building Docker image'
-    withCredentials([usernamePassword(credentialsId: 'tcdmvPipeLine', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t tcdmv/mvn-app:java-mvn-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'bmjenKey', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t bmchaar/my-app:java-mvn-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push tcdmv/mvn-app:java-mvn-2.0'
+        sh 'docker push bamchaar/my-app:java-mvn-2.0'
     }
 }
 def buildJar() {
