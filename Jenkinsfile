b6fb2724621f29243bfe338b73c5e796e74a7475
Abdel <bamchaar@gmail.com>
2023-03-18 15:28:03 -0400

    export DOCKER_HOST=unix:///var/run/docker.sock

--- Jenkinsfile
+++ Jenkinsfile
@@ -55,7 +55,7 @@ pipeline {
        
             steps {
 				script{
-				buildImage()
+				gv.buildImage()
 				}
 				
             }
--- script.groovy
+++ script.groovy
@@ -1,6 +1,7 @@
 def buildImage() {
     echo 'Building Docker image'
     withCredentials([usernamePassword(credentialsId: 'bamchaarJenkinsKey', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+		sh 'export DOCKER_HOST=unix:///var/run/docker.sock'
         sh 'docker build -t bamchaar/my-app:java-mvn-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push bamchaar/my-app:java-mvn-2.0'
